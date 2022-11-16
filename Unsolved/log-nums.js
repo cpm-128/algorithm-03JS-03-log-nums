d155bedad6949df4e4f242c9fc18808f431e1d8f
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-11-16 11:43:05 -0500

    passes test

--- Unsolved/log-nums.js
+++ Unsolved/log-nums.js
@@ -1,4 +1,12 @@
-// Write code to print all numbers from 1 to `num`
-// Assume `num` will be a positive number
+//TODO: Write code to print all numbers from 1 to `num`
+//! Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+
+    //console.log(num);
+
+    // print all numbers from 1-num (inclusive) in the consle
+    for (i = 1; i <= num; i++) {
+        console.log(i);
+    }
+};
